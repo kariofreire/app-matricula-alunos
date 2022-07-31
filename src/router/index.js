385099c439f4a9014adea46714c481e9fe922be4
Kario Freire <kariofreiretech@gmail.com>
2022-07-31 15:11:58 -0300

    Router - Adiciona rota com middware

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Login from '@/views/Login.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import Login from '@/views/Login.vue';
+import Guard from '@/services/middleware';
 
 Vue.use(VueRouter)
 
@@ -19,6 +20,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    beforeEnter: Guard.auth,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
